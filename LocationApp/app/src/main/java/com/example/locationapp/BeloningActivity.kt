43b881535570ea423d6841b8e2b6ad77312e9916
Weshley Gommeren <i488863@fhict.nl>
2024-03-25 15:54:02 +0100

    popup melding code beloning weergeven en sluiten werkend

--- LocationApp/app/src/main/java/com/example/locationapp/BeloningActivity.kt
+++ LocationApp/app/src/main/java/com/example/locationapp/BeloningActivity.kt
@@ -2,15 +2,16 @@ package com.example.locationapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.content.Context
+import android.view.Gravity
+import android.view.WindowManager
+import android.widget.ImageButton
+import android.widget.PopupWindow
 import androidx.appcompat.app.AppCompatActivity
 
 class BeloningActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_beloning)
-
-    }
 
     fun mainactivity(view: View) {
         startActivity(Intent(this, MainActivity::class.java))
@@ -19,4 +20,34 @@ class BeloningActivity : AppCompatActivity(){
     fun stamboomactivity(view: View) {
         startActivity(Intent(this, StamboomActivity::class.java))
     }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beloning)
+
+        val codeWeergevenButton = findViewById<ImageButton>(R.id.redbutton)
+
+        codeWeergevenButton.setOnClickListener {
+            val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val popupView = inflater.inflate(R.layout.activity_codeweergeven, findViewById(android.R.id.content), false)
+
+            val closeButton = popupView.findViewById<ImageButton>(R.id.codevoltooid)
+
+            val popupWindow = PopupWindow(
+                popupView,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT
+            )
+
+            popupWindow.isFocusable = true
+            popupWindow.showAtLocation(popupView, Gravity.BOTTOM, 0, 0)
+
+            closeButton.setOnClickListener {
+                popupWindow.dismiss()
+            }
+        }
+    }
+
+
 }
